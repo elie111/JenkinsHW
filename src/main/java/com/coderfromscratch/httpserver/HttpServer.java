0712a84cae99ff926fb3081f2350ddb8a54484ef
elie haddad <haddadelie12@gmail.com>
2022-11-14 21:11:11 +0200

    trying something

--- src/main/java/com/coderfromscratch/httpserver/HttpServer.java
+++ src/main/java/com/coderfromscratch/httpserver/HttpServer.java
@@ -95,6 +95,7 @@ public class HttpServer {
         a.loadConfigurationFile(temp2.toString());
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
 System.out.println("Using Port: " + conf.getPort());
+conf.setWebroot("localhost");
         System.out.println("Using WebRoot: " + conf.getWebroot());
 //        LOGGER.info("Using Port: " + conf.getPort());
 //        LOGGER.info("Using WebRoot: " + conf.getWebroot());
